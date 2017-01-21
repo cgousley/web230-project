5e0b887666bba7899fea375814ce10ac1cb015cd
Clayton G. Ousley <Clayton@ClaytonGOusley.com>
2017-01-21 00:00:56 -0500

    Update login.js
    better comments

--- controllers/admin/login.js
+++ controllers/admin/login.js
@@ -1,100 +1,89 @@
 var userModel = require('../../models').Users;
 var adminModel = require('../../models').Admins;
 
-// op = {};
 
 module.exports = {
-	/*LOGIN PAGE FIRST LOAD*/
-     index: function(req, res){
-          var url = require('url');
-          var url_parts = url.parse(req.url, true);
-          var query = url_parts.query;
-          
-           if(req.session.success && req.session.admin){
-        	res.redirect('../../admin/home');
-        }
-        // //If session is success but privlege is user, redirect to 401
-        // else if(req.session.success && req.session.user){
-        	   // res.redirect('../401');
-        // }
-        /*IF THERE IS NOT SUCCESS PROPERTY THEN SEND THE BACK TO LOGIN PAGE.*/
-        else{
-            // res.redirect('/admin/login/?error=1');
-          res.render('admin/login',{title: 'Admin - Login', heading: 'Home Page', blankBar: true, ackMessage: true});
-       	}
+/*LOGIN PAGE FIRST LOAD*/
+	index: function(req, res){
+  		var url = require('url');
+		var url_parts = url.parse(req.url, true);
+  		var query = url_parts.query;
+        
+		//If admin session, send to admin home
+   		if(req.session.success && req.session.admin){
+			res.redirect('../../admin/home');
+		}
 
-},
+		/*IF THERE IS NOT SUCCESS PROPERTY THEN SEND THE BACK TO LOGIN PAGE.*/
+        	else{
+          	res.render('admin/login',{title: 'Admin - Login', heading: 'Home Page', blankBar: true, ackMessage: true});
+       		}
+	},
 
 
-     access: function(req, res){
-         	
-                         var data = req.body.data;
-                        var data = JSON.parse(req.body.data);
+	access: function(req, res){
+ 		var data = req.body.data;
+		var data = JSON.parse(req.body.data);
 
-// 							Checks for the user email
-							adminModel.findOne({"email": data.email}, function(err, admin){
+		//Checks for the user email
+			adminModel.findOne({"email": data.email}, function(err, admin){
+				if (err){
+					console.log(err);
+				}
+				else {//if not found, alert user not found
+					if(admin === null){
+						res.send("admin not found");
+					}
+					else{// else if found, check for encrypted password
+						var bcrypt = require('bcrypt');
+						bcrypt.compare(data.password, admin.password, function(err, success){
 							if (err){
 								console.log(err);
 							}
-							else {//if not found, alert user not found
-								if(admin === null){
-									res.send("admin not found");
-								}
-								else{// else if found, check for encrypted password
-									var bcrypt = require('bcrypt');
-									bcrypt.compare(data.password, admin.password, function(err, success){
-										if (err){
-											console.log(err);
-										}
-										else if (success) {// if found, create session
+							else if (success) {// if found, create session
 											
-											
-											//If session already exists, add req.session.admin
-											if (req.session.success){
-											
-												req.session.admin = 'success';
-                         							var data = {};
+								//If session already exists, add req.session.admin
+								if (req.session.success){
+									req.session.admin = 'success';
+									var data = {};
 					 								                         							
-                         							//send user ID for addess and for inclusing in cart submit 
-                         							data._id = admin._id;
-                         							res.send(data);	
-												
-											}
-											
-											//If no session exists
-											else {
-												
-												req.session.regenerate(function(err){												
-													if (err){
-														console.log(err);
-													}
-													//IF SUCCESSFUL LOGIN CREATE SESSION SUCCESS PROPERTY AND REDIRECT TO ADMIN HOME PAGE
-	                   								else{//if session creation successful, send access approved
-	                   									console.log("success");
-	                         							req.session.success = 'access approved';
-	                         							req.session.admin = 'success';
-	                         							var data = {};
-														                         							
-	                         							//send user ID for addess and for inclusing in cart submit 
-	                         							data._id = admin._id;
-	                         							res.send(data);	
-                         							}
-												});
-												
-											}
-											
-										}
-										else {// password not found
-											res.send("admin not found");		
-										}
-									})
+									//send user ID for addess and for inclusing in cart submit 
+									data._id = admin._id;
+									res.send(data);	
 								}
+								//If no session exists
+									else {
+										req.session.regenerate(function(err){												
+											if (err){
+												console.log(err);
+											}					
+											//IF SUCCESSFUL LOGIN CREATE SESSION SUCCESS PROPERTY AND REDIRECT TO ADMIN HOME PAGE
+	                   								else{//if session creation successful, send access approved
+												console.log("success");
+	                         								req.session.success = 'access approved';
+	                         								req.session.admin = 'success';
+	                         								var data = {};
+														                         							
+	                         								//send user ID for addess and for inclusing in cart submit 
+	                         								data._id = admin._id;
+	                         								res.send(data);	
+                         								}
+										});
+												
+									}
+											
+								}
+							else {// password not found
+								res.send("admin not found");		
 							}
-						});
-     }, 
+						})
+					}
+				}
+			});
+		}, 
 
 
-    /*THIS IS THE LOGOUT PAGE WHERE THE SESSION IS DESTROYED*/
+	/*THIS IS THE LOGOUT PAGE WHERE THE SESSION IS DESTROYED*/
 	logout: function(req, res){
      	
 	 	//If there is only an admin session, destroy the entire session object
@@ -102,13 +91,13 @@ module.exports = {
      		
       		req.session.destroy(function(err){
 				if(err){
-            	    console.log(err);
- 	      		}
+            	    			console.log(err);
+				}
                	
-               	else{
-                    res.redirect('/');
-               }
-         	});
+				else{
+				    res.redirect('/');
+			       }
+			});
 
 		}
 		//If there is an admin and a user session, delete the admin session 
